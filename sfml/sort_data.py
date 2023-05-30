171d3c1280bd83a3cf27f6190d406bb65ae75be6
1822913 <peterkoegler@gmx.de>
2023-05-30 17:24:09 +0200

    new data set

--- sfml/sort_data.py
+++ sfml/sort_data.py
@@ -4,8 +4,10 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv("events.csv")
+print(len(data))
 data = data[data.location == 14]
-data.to_csv("pen.csv")
+print(len(data))
+# data.to_csv("pen.csv")
 # data = data[data.shot_outcome == 1]
 # data = data[data.event_type == 1]
 # goal = data[data.is_goal == 1]
