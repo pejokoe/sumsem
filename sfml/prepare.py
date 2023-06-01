452c4bbc1c8f1a14d97b61ed12b2aada33511cde
1822913 <peterkoegler@gmx.de>
2023-06-01 09:45:53 +0200

    new dataset sfml

--- sfml/prepare.py
+++ sfml/prepare.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 
-import pandas as pd
 import numpy as np
+import pandas as pd
+import plotly.express as px
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+from sklearn.utils import resample
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.feature_selection import SelectKBest, f_classif
+from imblearn.over_sampling import SMOTE
+import time
 
-apartments = pd.read_csv("immo_data.csv")
-apartments = apartments[["serviceCharge", "picturecount" ,"pricetrend", "telekomUploadSpeed", "totalRent", "yearConstructed", "baseRent", "livingSpace", "garden"]]
-apartments = apartments.dropna()
-print(apartments.head())
-apartments.to_csv("/home/swt/Programme/sumsem/sfml/immo_cleaned.csv")
+hospital = pd.read_csv("hospital.csv")
+hospital = hospital.select_dtypes(include=np.number)
+remove_columns = [col for col in hospital.columns if "pache" in col]
+hospital = hospital.drop(columns=["Unnamed: 83"])
+hospital = hospital.drop(columns=remove_columns)
+print(hospital.head())
+# x_new = SelectKBest(f_classif, k=3).fit_transform(hospital.drop(columns=["hospital_death"]), hospital.hospital_death)
+# print(x_new[:10])
+hospital.to_csv("hosp_cleaned.csv")
 print("done")
